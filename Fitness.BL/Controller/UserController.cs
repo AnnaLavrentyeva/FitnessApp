2c0ac0932126989d354a456cd234626c573724d1
Anna Lavrentyeva <ann.lvrntv@gmail.com>
2020-09-07 20:44:49 +0300

    User controller unit tests

--- Fitness.BL/Controller/UserController.cs
+++ Fitness.BL/Controller/UserController.cs
@@ -64,7 +64,7 @@ namespace Fitness.BL.Controller
             var formatter = new BinaryFormatter();
             using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
-                if (formatter.Deserialize(fs) is List<User> users)
+                if (fs.Length > 0 && formatter.Deserialize(fs) is List<User> users)
                 {
                     return  users;
                 }
@@ -85,6 +85,7 @@ namespace Fitness.BL.Controller
             var formatter = new BinaryFormatter();
             using(var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
+
                 formatter.Serialize(fs, Users);
             }
         }
